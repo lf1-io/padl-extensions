99595bb04df351315ddfbcecc119b813b629dec9
Sebastian Lee <sebastian@lf1.io>
2022-01-11 11:47:57 +0100

    Updating message when importing

--- padl_ext/__init__.py
+++ padl_ext/__init__.py
@@ -1,18 +1,21 @@
 try:
     from padl_ext.pytorch_lightning.prepare import PadlLightning
 except ImportError:
-    print("Please install pytorch lightning to use the connector")
+    print("Please install pytorch lightning dependencies "
+          "(pip install padl-extensions[pytorch_lightning]) to use the connector")
     pass
 
 try:
     from padl_ext.torchserve.prepare import prepare
     from padl_ext.torchserve.handler import PadlHandler
 except ImportError:
-    print("Please install torchserve to use the connector")
+    print("Please install torchserve dependencies "
+          "(pip install padl-extensions[torchserve]) to use the connector")
     pass
 
 try:
     from padl_ext.huggingface.convert import to_padl
 except ImportError:
-    print("Please install huggingface transformers to use connector")
+    print("Please install huggingface transformers dependencies "
+          "(pip install padl-extensions[huggingface]) to use connector")
     pass
